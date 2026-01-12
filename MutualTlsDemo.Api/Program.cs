a8dcd7d826507b51d639342af29c8937a53d69ed
Fredrik Klingenberg <fredrkl@gmail.com>
2026-01-12 13:21:16 +0100

    ✨ Vayapay client

--- MutualTlsDemo.Api/Program.cs
+++ MutualTlsDemo.Api/Program.cs
@@ -1,10 +1,25 @@
+using System.Security.Cryptography.X509Certificates;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+Console.WriteLine("Certificate Base64 from Configuration:");
+Console.WriteLine(builder.Configuration["Vayapay:ClientCertificate:CertBase64"]);
+
+Uri theuri = new(builder.Configuration["Vayapay:BaseUrl"]!);
+
+builder.Services.AddHttpClient<VayapayClient>(config => {
+    config.BaseAddress = theuri;
+  }).ConfigurePrimaryHttpMessageHandler(() => {
+    var handler = new HttpClientHandler();
+    var base64Cert = builder.Configuration["Vayapay:ClientCertificate:CertBase64"];
+    var cert = new X509Certificate2(Convert.FromBase64String(base64Cert!));
+    return handler;
+});
 
 var app = builder.Build();
 
---
+++ MutualTlsDemo.Api/VayapayClient.cs
@@ -0,0 +1,16 @@
+public class VayapayClient
+{
+    private readonly HttpClient _httpClient;
+
+    public VayapayClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<string> GetDataAsync()
+    {
+        var response = await _httpClient.GetAsync("https://api.vayapay.com/data");
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
+    }
+}
