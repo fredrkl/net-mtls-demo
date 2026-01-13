ca118142344ed5804faa46b324c6f6e1ba6d151e
Fredrik Klingenberg <fredrkl@gmail.com>
2026-01-13 11:48:41 +0100

    ♻️ Use DI resolved configuration

--- MutualTlsDemo.Api/Program.cs
+++ MutualTlsDemo.Api/Program.cs
@@ -9,37 +9,39 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-Console.WriteLine("Certificate Base64 from Configuration:");
-Console.WriteLine(builder.Configuration["Vayapay:ClientCertificate:CertBase64"]);
+//Console.WriteLine("Certificate Base64 from Configuration:");
+//Console.WriteLine(builder.Configuration["Vayapay:ClientCertificate:CertBase64"]);
+//
+//// CERTIFICATE
+//var b64 = builder.Configuration["Vayapay:ClientCertificate:CertBase64"];
+//var bytes = Convert.FromBase64String(b64!);
+//var text = Encoding.UTF8.GetString(bytes);
+//
+//Console.WriteLine(text.StartsWith("-----BEGIN CERTIFICATE-----")
+//    ? "CertBase64 decodes to PEM ✅"
+//    : "CertBase64 does NOT decode to PEM ❌");
+//
+//// CERTIFICATE KEY
+//var keyB64 = builder.Configuration["Vayapay:ClientCertificate:CertKeyBase64"];
+//var keyText = Encoding.UTF8.GetString(Convert.FromBase64String(keyB64!));
+//
+//Console.WriteLine(keyText.StartsWith("-----BEGIN")
+//    ? "KeyBase64 decodes to PEM ✅"
+//    : "KeyBase64 does NOT decode to PEM ❌");
+//
+////Console.WriteLine("Certificate KEY Base64 from Configuration:");
+////Console.WriteLine(builder.Configuration["Vayapay:ClientCertificate:CertKeyBase64"]);
 
-// CERTIFICATE
-var b64 = builder.Configuration["Vayapay:ClientCertificate:CertBase64"];
-var bytes = Convert.FromBase64String(b64!);
-var text = Encoding.UTF8.GetString(bytes);
 
-Console.WriteLine(text.StartsWith("-----BEGIN CERTIFICATE-----")
-    ? "CertBase64 decodes to PEM ✅"
-    : "CertBase64 does NOT decode to PEM ❌");
-
-// CERTIFICATE KEY
-var keyB64 = builder.Configuration["Vayapay:ClientCertificate:CertKeyBase64"];
-var keyText = Encoding.UTF8.GetString(Convert.FromBase64String(keyB64!));
-
-Console.WriteLine(keyText.StartsWith("-----BEGIN")
-    ? "KeyBase64 decodes to PEM ✅"
-    : "KeyBase64 does NOT decode to PEM ❌");
-
-//Console.WriteLine("Certificate KEY Base64 from Configuration:");
-//Console.WriteLine(builder.Configuration["Vayapay:ClientCertificate:CertKeyBase64"]);
-
-Uri theuri = new(builder.Configuration["Vayapay:BaseUrl"]!);
-
-builder.Services.AddHttpClient<VayapayClient>(config => {
-    config.BaseAddress = theuri;
-  }).ConfigurePrimaryHttpMessageHandler(() => {
+builder.Services.AddHttpClient<VayapayClient>((sp, client) => {
+    var config = sp.GetRequiredService<IConfiguration>();
+    Uri theuri = new(config["Vayapay:BaseUrl"]!);
+    client.BaseAddress = theuri;
+  }).ConfigurePrimaryHttpMessageHandler((sp) => {
+    var config = sp.GetRequiredService<IConfiguration>();
     var handler = new HttpClientHandler();
-    var base64Cert = builder.Configuration["Vayapay:ClientCertificate:CertBase64"];
-    var base64CertKey = builder.Configuration["Vayapay:ClientCertificate:CertKeyBase64"];
+    var base64Cert = config["Vayapay:ClientCertificate:CertBase64"];
+    var base64CertKey = config["Vayapay:ClientCertificate:CertKeyBase64"];
 
     var certificate = X509CertificateLoader.LoadCertificate(Convert.FromBase64String(base64Cert!));
 
